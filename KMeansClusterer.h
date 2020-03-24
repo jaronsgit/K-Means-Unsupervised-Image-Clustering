4d31ca03c463d030a428613ae96a1b6fa5efcc97
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-03-24 10:12:15 +0200

    Added default constructor and parameterised constructor to KMeansClusterer.h

--- KMeansClusterer.h
+++ KMeansClusterer.h
@@ -1,5 +1,9 @@
 #ifndef KMEANSCLUSTERER_H
 #define KMEANSCLUSTERER_H
+#include <string>
+#include <vector>
+#include "ClusterImage.h"
+#include <dirent.h>
 
 namespace CHNJAR003
 {
@@ -7,9 +11,19 @@ namespace CHNJAR003
 class KMeansClusterer
 {
 private:
-    /* data */
+    std::string datasetDir;
+    std::string outputFileName;
+    int binSize;
+    int numClusters;
+    std::vector<std::vector<ClusterImage>> clusters; //2D vector with list of images associated with each cluster
+    std::vector<ClusterImage> images;
+
 public:
+    typedef unsigned char u_char;
     KMeansClusterer(void);
+    KMeansClusterer(const std::string &dataset, const std::string &output, const int numClusters, const int binSize);
+    std::vector<ClusterImage> readInImages(const std::string &datasetDir);
+    std::vector<u_char> convertToGreyscale(std::vector<u_char> rgbValues);
     ~KMeansClusterer();
 };
 
