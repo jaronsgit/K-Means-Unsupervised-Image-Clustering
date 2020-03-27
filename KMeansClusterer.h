2deda3238141b4423aeadf0129a7c90b4f772180
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-03-27 14:03:21 +0200

    Added declaration for findNearestCluster() method to KMeansClusterer.h. Modified images vector to store shared pointer and clusters vector to store unique pointers.

--- KMeansClusterer.h
+++ KMeansClusterer.h
@@ -2,8 +2,9 @@
 #define KMEANSCLUSTERER_H
 #include <string>
 #include <vector>
+#include <memory>
 #include "ClusterImage.h"
-#include <dirent.h>
+#include "Cluster.h"
 
 namespace CHNJAR003
 {
@@ -15,16 +16,20 @@ private:
     std::string outputFileName;
     int binSize;
     int numClusters;
-    std::vector<std::vector<ClusterImage>> clusters; //2D vector with list of images associated with each cluster
-    std::vector<ClusterImage> images;
+    std::vector<std::shared_ptr<ClusterImage>> images;
+    std::vector<std::unique_ptr<Cluster>> clusters;
+
+    int findNearestCluster(std::shared_ptr<ClusterImage> image); //return the id number of the nearest cluster
 
 public:
     typedef unsigned char u_char;
     KMeansClusterer(void);
     KMeansClusterer(const std::string &dataset, const std::string &output, const int numClusters, const int binSize);
-    std::vector<ClusterImage> readInImages(const std::string &datasetDir);
+    std::vector<std::shared_ptr<ClusterImage>> readInImages(const std::string &datasetDir);
     std::vector<u_char> convertToGreyscale(std::vector<u_char> rgbValues);
     ~KMeansClusterer();
+
+    friend std::ostream &operator<<(std::ostream &os, const KMeansClusterer &kt);
 };
 
 } // namespace CHNJAR003
